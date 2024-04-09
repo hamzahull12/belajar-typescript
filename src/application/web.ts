8093cd7ea46c0b1a177f54c13fc8df5957fb7c83
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-09 16:35:02 +0700

    add service and validation

---
+++ src/application/web.ts
@@ -0,0 +1,4 @@
+import expres from "express";
+
+export const web = expres();
+web.use(expres.json());
---
+++ src/service/user-service.ts
@@ -0,0 +1,37 @@
+import { prismaClient } from "../application/database";
+import { ResponseError } from "../exeption/response-error";
+import { CreateUserRequest, UserResponse, mapUserModel } from "../model/user-model";
+import { UserValidation } from "../validation/users/user-validation";
+import { Validation } from "../validation/users/validation";
+import bcrypt from "bcrypt";
+
+  export class UserService {
+    //method for regis
+    static async register(request: CreateUserRequest) : Promise<UserResponse> {
+
+      // validation req data
+      const requestRegister = Validation.validate(UserValidation.REGISTER, request);
+
+      // verify username exist
+      const verifyUsernameExist = await prismaClient.user.count({
+        where: {
+          username: requestRegister.username
+        }
+      });
+      
+      // check is username aready exist
+      if (verifyUsernameExist !== 0) {
+        throw new ResponseError('username already exits ', 400);  
+      }
+
+      // hash password to bcrypt
+      requestRegister.password = await bcrypt.hash(requestRegister.password, 10);
+
+      // final insert into req payload to users table with prisma orm
+      const user = await prismaClient.user.create({
+        data: requestRegister
+      });
+
+      return mapUserModel(user);
+    }
+  }
